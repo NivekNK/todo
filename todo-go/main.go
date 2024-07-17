02061b549ec31cce2eb6591dabdb64ced9b45285
NivekNK <kevin.rojas.v@mail.pucv.cl>
2024-07-16 23:42:52 -0400

    Initial interface with bubbletea

--- todo-go/main.go
+++ todo-go/main.go
@@ -5,14 +5,105 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
 )
 
-// App struct to hold each app's data
+type Installer struct {
+	Installer string `json:"installer"`
+	Id        string `json:"id"`
+}
+
 type App struct {
-	Name          string   `json:"name"`
-	WindowInstall string   `json:"window_install"`
-	LinuxInstall  string   `json:"linux_install"`
-	Prerequisites []string `json:"prerequisites"`
+	Name          string      `json:"name"`
+	Windows       interface{} `json:"windows"`
+	Linux         interface{} `json:"linux"`
+	Prerequisites []string    `json:"prerequisites"`
+}
+
+type InstallApp struct {
+	Name      string `json:"name"`
+	Installed bool   `json:"installed"`
+	Version   string `json:"version"`
+}
+
+type PrettyInstallApp struct {
+	title       string
+	installed   bool
+	description string
+}
+
+// Implement the list.Item interface
+func (app PrettyInstallApp) FilterValue() string {
+	return app.title
+}
+
+func (app PrettyInstallApp) Title() string {
+	return app.title
+}
+
+func (app PrettyInstallApp) Description() string {
+	return app.description
+}
+
+type Model struct {
+	list list.Model
+}
+
+func New() *Model {
+	return &Model{}
+}
+
+func (model *Model) initList(width int, height int) {
+	model.list = list.New([]list.Item{}, list.NewDefaultDelegate(), width, height)
+	model.list.Title = "Apps"
+
+	// Convert the array of InstallApp to a slice of list.Item
+	// items := make([]list.Item, len(installApps))
+	// for i, app := range installApps {
+	// 	items[i] = PrettyInstallApp{
+	// 		name:      app.Name,
+	// 		installed: app.Installed,
+	// 		version:   app.Version,
+	// 	}
+	// }
+
+	model.list.SetItems([]list.Item{
+		PrettyInstallApp{
+			title:       "GoLang",
+			description: "none",
+			installed:   false,
+		},
+	})
+}
+
+func (model Model) Init() tea.Cmd {
+	return nil
+}
+
+func (model Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		model.initList(msg.Width, msg.Height)
+	}
+
+	var cmd tea.Cmd
+	model.list, cmd = model.list.Update(msg)
+	return model, cmd
+}
+
+func (model Model) View() string {
+	return model.list.View()
+}
+
+func isInstallApp(installApps []InstallApp, appName string) bool {
+	for _, installApp := range installApps {
+		if installApp.Name == appName {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
@@ -62,9 +153,7 @@ func main() {
 			fmt.Println("Error creating file:", err)
 			return
 		}
-
-		// file.WriteString()
-
+		file.WriteString("[]")
 		file.Close()
 		fmt.Println("File 'nk-apps.json' created:", filePath)
 	} else if err != nil {
@@ -73,4 +162,53 @@ func main() {
 	} else {
 		fmt.Println("File 'nk-apps.json' already exists:", filePath)
 	}
+
+	// Read the existing file content
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		fmt.Println("Error reading file 'nk-apps.json':", err)
+		return
+	}
+
+	// Parse the JSON content into a slice of InstallApp structs
+	var installApps []InstallApp
+	err = json.Unmarshal(data, &installApps)
+	if err != nil {
+		fmt.Println("Error unmarshalling 'nk-apps.json':", err)
+		return
+	}
+
+	for _, app := range apps {
+		if !isInstallApp(installApps, app.Name) {
+			newInstallApp := InstallApp{
+				Name:      app.Name,
+				Installed: false,
+				Version:   "none",
+			}
+			installApps = append(installApps, newInstallApp)
+		}
+	}
+
+	model := New()
+	program := tea.NewProgram(model)
+	if _, err := program.Run(); err != nil {
+		fmt.Println("Program error:", err)
+		os.Exit(1)
+	}
+
+	// Marshal the modified data into JSON with indentation
+	newData, err := json.MarshalIndent(installApps, "", "    ")
+	if err != nil {
+		fmt.Println("Error marshalling JSON:", err)
+		return
+	}
+
+	// Overwrite the file with the new content
+	err = os.WriteFile(filePath, newData, 0644)
+	if err != nil {
+		fmt.Println("Error writing to file 'nk-apps.json':", err)
+		return
+	}
+
+	fmt.Println("'nk-apps.json' Updated!")
 }
